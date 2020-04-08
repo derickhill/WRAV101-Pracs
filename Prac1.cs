58f8e0ea23bbe16cb7730b4e067a1d3727491167
Derick Hill <s221419535@mandela.ac.za>
2020-04-08 11:24:43 +0200

    Prac 1 complete.

---
+++ Prac1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WRAV101_Pracs
+{
+    class Prac1
+    {
+        static void Main(string[] args)
+        {
+            int x, y, sum;
+
+            x = 7;
+            y = 3;
+            sum = x + y;
+
+            Console.WriteLine("The sum of {0} and {1} is {2}.", x, y, sum);
+            Console.ReadLine();
+        }
+    }
+}
