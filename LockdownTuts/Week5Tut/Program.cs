768e37c63fb3ada5f515c59eb5b9db66a204f189
Derick Hill <s221419535@mandela.ac.za>
2020-06-13 07:35:57 +0200

    Practical 11 complete.

---
+++ LockdownTuts/Week5Tut/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Week5Tut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double maxAverage = Double.NegativeInfinity;
+            int count = 0;
+            
+            for(int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("\nAthlete {0}:", i);
+                
+                double ave = getAverage();
+
+                if(ave > 7)
+                {
+                    count++;
+                }
+
+                UpdateMax(ave, ref maxAverage);
+            }
+
+            Display(count, maxAverage);
+        }
+
+        static public int getValidScore()
+        {
+            int input;
+
+            do
+            {
+                try
+                {
+                    input = int.Parse(Console.ReadLine());
+                }
+                catch(FormatException)
+                {
+                    input = -1;
+                }
+            }
+            while(input < 0 || input > 10);
+
+            return input;
+        }
+
+        static public double getAverage()
+        {
+            int sum = 0;
+
+            for(int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine("Enter score {0}:", i);
+                sum += getValidScore();
+            }
+
+            return sum/3.0;
+        }
+
+        static public void UpdateMax(double a, ref double max)
+        {
+            if(a > max)
+            {
+                max = a;
+            }
+        }
+
+        static public void Display(int count, double max)
+        {
+            Console.WriteLine("Number average scores above 7: {0}\nHighest average score: {1}", count, max);
+        }
+    }
+}
---
+++ Prac11/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Prac11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tempArr = new int[10];
+            
+            read10Integers(tempArr);
+
+            Display(tempArr);
+
+            Console.WriteLine("Average temperature: {0} degrees Celsius", getAvgTemperature(tempArr));
+        }
+
+        static public int getValidTemperature()
+        {
+            int temp;
+
+            do
+            {
+                Console.WriteLine("Enter temperature:");
+                try
+                {
+                    temp = int.Parse(Console.ReadLine());
+                }
+                catch(FormatException)
+                {
+                    temp = 0;
+                }
+            }
+            while(temp <= 0 || temp >= 45);
+
+            return temp;
+        }
+
+        static public void Display(int[] list)
+        {
+            foreach(int i in list)
+            {
+                Console.WriteLine(i + " degrees Celsius");
+            }
+        }
+
+        static public void read10Integers(int[] list)
+        {
+            for(int i = 0; i < list.Length; i++)
+            {
+                list[i] = getValidTemperature();
+            }
+        }
+
+        static public double getAvgTemperature(int[] list)
+        {
+            int sum = 0;
+
+            foreach(int i in list)
+            {
+                sum += i;
+            }
+
+            return sum/(list.Length * 1.0);
+        }
+    }
+}
