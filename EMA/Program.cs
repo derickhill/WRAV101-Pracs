b1faeb57a9343b17d91efc4f8ef815b4e308688f
Derick Hill <s221419535@mandela.ac.za>
2020-08-01 09:48:05 +0200

    EMA complete.

---
+++ EMA/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace EMA
+{
+    class Program
+    {
+        static int[] list = new int[10];
+        static int NrEl = 0;
+        static void Main(string[] args)
+        {
+            ReadMarksIntoArray(list, ref NrEl);
+            
+            displayGoodMarks(list, NrEl);
+        }
+
+        static public int getValidNumber(int a, int b)
+        {
+            int nr;
+            do
+            {
+                try
+                {
+                    nr = int.Parse(Console.ReadLine());
+                }
+                catch(FormatException)
+                {
+                    nr = a - 1;
+                }
+            }
+            while(nr > b || nr < a);
+
+            return nr;
+        }
+
+        static public void ReadMarksIntoArray (int[] list, ref int NrEl)
+        {
+            Console.WriteLine("How many marks need to be read in?");
+
+            int noMarks = getValidNumber(0, 10);
+
+            for(int i = 0; i < noMarks; i++)
+            {
+                Console.WriteLine("Mark {0}:", i+1);
+                list[NrEl] = getValidNumber(0, 100);
+                NrEl++;
+            }
+        }
+
+        static public double getAverage(int[] list, int NrEl)
+        {
+            int sum = 0;
+
+            for(int i = 0; i < NrEl; i++)
+            {
+                sum += list[i];
+            }
+
+            return sum/(NrEl*1.0);
+        }
+
+        static public void displayGoodMarks (int[] list, int NrEl)
+        {
+            double ave = getAverage(list, NrEl);
+
+            Console.WriteLine("Marks above average:");
+
+            for(int i = 0; i < NrEl; i++)
+            {
+                if(list[i] > ave)
+                {
+                    Console.WriteLine(list[i]);
+                }
+            }
+        }
+    }
+}
---
+++ LockdownTuts/Week6Tut/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Week6Tut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] salesArr = new int[10];
+            int NrEl = 0;
+
+            Console.WriteLine("How many salesmen sold masks this week?");
+            int noSalesmen = getPositiveNumber();
+
+            for(int i = 0; i < noSalesmen; i++)
+            {
+                ReadSalesIntoArray(salesArr, ref NrEl);
+            }
+
+            Display(salesArr, NrEl);
+
+            DisplayStatement(getSum(salesArr, NrEl));
+        }
+
+        static public int getPositiveNumber()
+        {
+            int input;
+
+            do
+            {
+                try
+                {
+                    input = int.Parse(Console.ReadLine());
+                }
+                catch (System.FormatException)
+                {
+                    input = 0;
+                }
+            }
+            while(input <= 0);
+
+            return input;
+        }
+
+        static public void ReadSalesIntoArray (int[] list, ref int NrEl)
+        {
+            Console.WriteLine("Number of masks for salesman {0}:", NrEl+1);
+            list[NrEl] = getPositiveNumber();
+            NrEl++;
+        }
+
+        static public void Display(int[] list, int NrEl)
+        {
+            for(int i = 0; i < NrEl; i++)
+            {
+                Console.WriteLine("Agent {0}: {1} masks", i+1, list[i]);
+            }
+        }
+
+        static public int getSum (int[] list, int NrEl)
+        {
+            int sum = 0;
+            
+            for(int i = 0; i < NrEl; i++)
+            {
+                sum += list[i];
+            }
+
+            return sum;
+        }
+
+        static public void DisplayStatement(int TotalMasks)
+        {
+            Console.WriteLine("Total masks sold: {0}\nTotal income: {1:C}", TotalMasks, TotalMasks * 20.0);
+        }
+
+        
+    }
+}
